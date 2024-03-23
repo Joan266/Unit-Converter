29d40df1ad735ce8edfd46329f5b3e552221ca84
Joan <joanalemany26@gmail.com>
2024-03-23 12:18:31 +0100

    reverse unit added clicking horizontal white arrows

--- client/src/components/unitConverter.jsx
+++ client/src/components/unitConverter.jsx
@@ -2,20 +2,20 @@ import { useState, useEffect } from 'react';
 
 function UnitConverter() {
   const conversionOperations = [
-    { initialUnit: 'km', finalUnit: 'miles', conversionType: 'km_to_miles', conversionFactor: 0.621371 },
-    { initialUnit: 'miles', finalUnit: 'km', conversionType: 'miles_to_km', conversionFactor: 1.60934 },
-    { initialUnit: 'm', finalUnit: 'feet', conversionType: 'm_to_feet', conversionFactor: 3.28084 },
-    { initialUnit: 'feet', finalUnit: 'm', conversionType: 'feet_to_m', conversionFactor: 0.3048 },
-    { initialUnit: 'cm', finalUnit: 'inches', conversionType: 'cm_to_inches', conversionFactor: 0.393701 },
-    { initialUnit: 'inches', finalUnit: 'cm', conversionType: 'inches_to_cm', conversionFactor: 2.54 }
-  ];
+    { id: 0, initialUnit: 'km', finalUnit: 'miles', conversionType: 'km_to_miles', conversionFactor: 0.621371192237334 },
+    { id: 1, initialUnit: 'miles', finalUnit: 'km', conversionType: 'miles_to_km', conversionFactor: 1.609344 },
+    { id: 2, initialUnit: 'm', finalUnit: 'feet', conversionType: 'm_to_feet', conversionFactor: 3.280839895013123 },
+    { id: 3, initialUnit: 'feet', finalUnit: 'm', conversionType: 'feet_to_m', conversionFactor: 0.3048 },
+    { id: 4, initialUnit: 'cm', finalUnit: 'inches', conversionType: 'cm_to_inches', conversionFactor: 0.3937007874015748 },
+    { id: 5, initialUnit: 'inches', finalUnit: 'cm', conversionType: 'inches_to_cm', conversionFactor: 2.54 }
+];
   const [ conversionOperation, setConversionOperation ] = useState(conversionOperations[0]);
-  const [ initialNumber,setInitialNumber ] = useState(0);
-  const [ finalNumber,setFinalNumber ] = useState(0);
+  const [ initialNumber, setInitialNumber ] = useState(0);
+  const [ finalNumber, setFinalNumber ] = useState(0);
 
   const handleConversionOperationChange = (event) => {
-    const conversionOperationIndex = event.target.value;
-    setConversionOperation(conversionOperations[conversionOperationIndex]);
+    const index = event.target.value;
+    setConversionOperation(conversionOperations[index]);
   };
   
   const handleInitialNumberChange = (event) => {
@@ -23,10 +23,18 @@ function UnitConverter() {
     setInitialNumber(initialNumber);
   };
 
+  function handleReverseConvertion()  {
+    setInitialNumber(finalNumber);
+    if (conversionOperation.id % 2 === 0)
+      setConversionOperation(conversionOperations[conversionOperation.id + 1]);
+    else
+      setConversionOperation(conversionOperations[conversionOperation.id - 1]);
+  };
+
   useEffect(() => {
     function unitConversion() {
       const finalNumber = initialNumber * conversionOperation.conversionFactor;
-      setFinalNumber(Math.floor(finalNumber*100)/100);
+      setFinalNumber(finalNumber);
     }
     unitConversion()
   }, [conversionOperation,initialNumber]);
@@ -38,17 +46,18 @@ function UnitConverter() {
       </div>
       <div className="machine">
         <div>
-          <select onChange={handleConversionOperationChange}className='text-small'>
-            <option value={0}>km → miles</option>
-            <option value={1}>miles → km</option>
-            <option value={2}>m → feet</option>
-            <option value={3}>feet → m</option>
-            <option value={4}>cm → inches</option>
-            <option value={5}>inches → cm</option>
+          <select onChange={handleConversionOperationChange} value={conversionOperation.id} className='text-small'>
+            {conversionOperations.map(operation => (
+              <option key={operation.id} value={operation.id}>
+                {`${operation.initialUnit} → ${operation.finalUnit}`}
+              </option>
+            ))}
           </select>
-          <img src="icons/white_arrows.svg" alt="White arrows icon" />
+          <div className='reverse-convertion' onClick={handleReverseConvertion}>
+            <img src="icons/white_arrows.svg" alt="White arrows icon" />
+          </div>
         </div>
-        <div>
+        <div style={{minWidth:"284px"}}>
           <input type="number" name="distance_to_convert" className='text-small' value={initialNumber} onChange={handleInitialNumberChange} />
           <span className="initialunit text-small">
             {conversionOperation.initialUnit}
@@ -56,10 +65,12 @@ function UnitConverter() {
         </div>
       </div>
       <div className="results">
-        <img src="icons/heart.svg" alt="Heart icon" />
-        <div>
+        <div className='heart-container'>
+         <img src="icons/heart.svg" alt="Heart icon" />
+        </div>
+        <div style={{minWidth:"84px"}}>
           <span className="num text-bigger">
-            {finalNumber}
+            {Math.floor(finalNumber*100)/100}
           </span>
           <span className="finalunit text-medium">
             {conversionOperation.finalUnit}
