4f4003d72445810db6010ff9e8bf4834488e57cf
Joan <joanalemany26@gmail.com>
2024-03-24 12:39:11 +0100

    added routes controller paths

--- server/conversionController.js
+++ server/conversionController.js
@@ -1,101 +1,57 @@
 const mongoose = require('mongoose');
 const Conversion = require('./conversionModel.js');
 
-module.exports = conversionController =  {
+module.exports = conversionController = {
   add: async (req, res) => {
-    const {content, userId, postId, postImageFileId } = req.body
-    console.log(req.body)
+    const data = req.body;
+    console.log(data);
     try {
-      const post = await Post.create({ _id: new mongoose.Types.ObjectId(), content});
-      await User.findByIdAndUpdate(
-        userId,
-        { $push: { posts: post._id } }
-      );
-      if(postId){
-        post.parentPost = postId
-        await Post.findByIdAndUpdate(
-          postId,
-          { $push: { comments: post._id }, $inc: { commentsCount: 1 } },
-        );
-      }
-      post.user = userId;
-      post.postImageFileId = postImageFileId || null;
-      await post.save();
-      if(post){
-        res.status(200).json({_id: post._id})
-      }else {
-        console.log(`Post create operation failed`)
-        res.status(404).json({ error: "Post create operation failed" })
+      const conversion = await Conversion.create(data); 
+      if (conversion) {
+        console.log(`Conversion addition operation succeeded`);
+        res.status(200).json({ error: null });
+      } else {
+        console.log(`Conversion addition operation failed`);
+        res.status(404).json({ error: "Conversion create operation failed" }); 
       }
     } catch (error) {
-      console.log(`Error: ${error}`)
+      console.log(`Error: ${error}`);
       res.status(400).json({
         error: 'Bad Request',
-      })
+      });
     }
   },
   delete: async (req, res) => {
-    const {content, userId, postId, postImageFileId } = req.body
-    console.log(req.body)
+    const conversionId = req.params.id; 
+    console.log(conversionId);
     try {
-      const post = await Post.create({ _id: new mongoose.Types.ObjectId(), content});
-      await User.findByIdAndUpdate(
-        userId,
-        { $push: { posts: post._id } }
-      );
-      if(postId){
-        post.parentPost = postId
-        await Post.findByIdAndUpdate(
-          postId,
-          { $push: { comments: post._id }, $inc: { commentsCount: 1 } },
-        );
-      }
-      post.user = userId;
-      post.postImageFileId = postImageFileId || null;
-      await post.save();
-      if(post){
-        res.status(200).json({_id: post._id})
-      }else {
-        console.log(`Post create operation failed`)
-        res.status(404).json({ error: "Post create operation failed" })
+      const deletedConversion = await Conversion.findByIdAndDelete(conversionId); 
+      if (deletedConversion) {
+        res.status(200).json({ _id: deletedConversion._id }); 
+      } else {
+        res.status(404).json({ error: "Conversion not found" }); 
       }
     } catch (error) {
-      console.log(`Error: ${error}`)
+      console.log(`Error: ${error}`);
       res.status(400).json({
         error: 'Bad Request',
-      })
+      });
     }
   },
   conversions: async (req, res) => {
-    const {content, userId, postId, postImageFileId } = req.body
-    console.log(req.body)
     try {
-      const post = await Post.create({ _id: new mongoose.Types.ObjectId(), content});
-      await User.findByIdAndUpdate(
-        userId,
-        { $push: { posts: post._id } }
-      );
-      if(postId){
-        post.parentPost = postId
-        await Post.findByIdAndUpdate(
-          postId,
-          { $push: { comments: post._id }, $inc: { commentsCount: 1 } },
-        );
-      }
-      post.user = userId;
-      post.postImageFileId = postImageFileId || null;
-      await post.save();
-      if(post){
-        res.status(200).json({_id: post._id})
-      }else {
-        console.log(`Post create operation failed`)
-        res.status(404).json({ error: "Post create operation failed" })
+      const conversions = await Conversion.find().sort({ createdAt: -1 }); 
+      if (conversions.length > 0) {
+        res.status(200).json(conversions);
+      } else {
+        console.log(`Conversions empty`);
+        res.status(200).json([]);
       }
     } catch (error) {
-      console.log(`Error: ${error}`)
+      console.log(`Error: ${error}`);
       res.status(400).json({
         error: 'Bad Request',
-      })
+      });
     }
   }
-}
+};
